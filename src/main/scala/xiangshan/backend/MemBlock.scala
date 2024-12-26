ebc7e128ec44438dfbdc4afc51ae89e4a842bb88
sinceforYy <1017657683@qq.com>
2024-12-26 17:10:38 +0800

    feat(TopDown): add TopDown PMU Events

--- src/main/scala/xiangshan/backend/MemBlock.scala
+++ src/main/scala/xiangshan/backend/MemBlock.scala
@@ -187,6 +187,7 @@ class MemCoreTopDownIO extends Bundle {
   val robHeadLoadMSHR = Output(Bool())
 }
 
+
 class fetch_to_mem(implicit p: Parameters) extends XSBundle{
   val itlb = Flipped(new TlbPtwIO())
 }
@@ -350,6 +351,11 @@ class MemBlockInlinedImp(outer: MemBlockInlined) extends LazyModuleImp(outer)
     val traceCoreInterfaceBypass = new Bundle{
       val fromBackend = Flipped(new TraceCoreInterface(hasOffset = true))
       val toL2Top     = new TraceCoreInterface
+    }
+
+    val topDownInfo = new Bundle {
+      val fromL2Top = Input(new TopDownFromL2Top)
+      val toBackend = Flipped(new TopDownInfo)
     }
   })
 
@@ -1992,6 +1998,13 @@ class MemBlockInlinedImp(outer: MemBlockInlined) extends LazyModuleImp(outer)
   io.debugTopDown.toCore.robHeadLoadMSHR := lsq.io.debugTopDown.robHeadLoadMSHR
   dcache.io.debugTopDown.robHeadOtherReplay := lsq.io.debugTopDown.robHeadOtherReplay
   dcache.io.debugRolling := io.debugRolling
+
+  lsq.io.noUopsIssued := io.topDownInfo.toBackend.noUopsIssued
+  io.topDownInfo.toBackend.lqEmpty := lsq.io.lqEmpty
+  io.topDownInfo.toBackend.sqEmpty := lsq.io.sqEmpty
+  io.topDownInfo.toBackend.l1Miss := dcache.io.l1Miss
+  io.topDownInfo.toBackend.l2TopMiss.l2Miss := io.topDownInfo.fromL2Top.l2Miss
+  io.topDownInfo.toBackend.l2TopMiss.l3Miss := io.topDownInfo.fromL2Top.l3Miss
 
   val hyLdDeqCount = PopCount(io.ooo_to_mem.issueHya.map(x => x.valid && FuType.isLoad(x.bits.uop.fuType)))
   val hyStDeqCount = PopCount(io.ooo_to_mem.issueHya.map(x => x.valid && FuType.isStore(x.bits.uop.fuType)))
